a8ce2acf9716737a9ecc131507bb16407ea300ef
GalBachar3 <77277319+GalBachar3@users.noreply.github.com>
2024-02-23 07:52:58 +0200

    add authentication middleware

--- server/src/api/cocktails/cocktails.router.js
+++ server/src/api/cocktails/cocktails.router.js
@@ -1,11 +1,12 @@
 import { AsyncRouter } from 'express-async-router';
 import { createCocktail, getCocktails, deleteCocktail, updateCocktail } from './cocktails.controller.js';
+import { protect } from "../../middlewares/auth.middleware.js";
 
 const router = AsyncRouter();
 
 router.get('/', getCocktails);
-router.post('/', createCocktail);
-router.put('/:id', updateCocktail);
-router.delete('/:id', deleteCocktail);
+router.post('/', protect, createCocktail);
+router.put('/:id', protect, updateCocktail);
+router.delete('/:id', protect, deleteCocktail);
 
 export default router;
--- server/src/api/users/user.routes.js
+++ server/src/api/users/user.routes.js
@@ -1,10 +1,11 @@
 import { AsyncRouter } from 'express-async-router';
 import { deleteUser, getUsers, updateUser } from './user.controller.js';
+import { protect } from "../../middlewares/auth.middleware.js";
 
 const router = AsyncRouter();
 
 router.get('/', getUsers);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.put('/:id', protect, updateUser);
+router.delete('/:id', protect, deleteUser);
 
 export default router;
