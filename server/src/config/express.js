1b06404b5a62825da3cbed1e8b0c70dcddc66b0a
GalBachar3 <77277319+GalBachar3@users.noreply.github.com>
2024-03-08 11:42:39 +0200

    add dirname

--- server/src/config/express.js
+++ server/src/config/express.js
@@ -7,6 +7,7 @@ import multer from 'multer';
 import path from 'path';
 import { configSwagger } from './swagger.js';
 
+const dirname = path.resolve();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
