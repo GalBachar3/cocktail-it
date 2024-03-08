fe11b3ec017c94b1eb27d4680ecbbd63697d2e1f
GalBachar3 <77277319+GalBachar3@users.noreply.github.com>
2024-03-08 14:27:48 +0200

    fix vite config for api

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['@mui/icons-material/Home'],
+    },
+    outDir: 'dist',
+  },
   plugins: [react()],
-})
+  loader: { '.js': 'jsx' },
+  define: {
+    global: 'globalThis',
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/auth': 'http://localhost:3000',
+    },
+  },
+});
