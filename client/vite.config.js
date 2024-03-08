3c696aceec37687d9f290d5f65f4800de7faa08f
GalBachar3 <77277319+GalBachar3@users.noreply.github.com>
2024-03-08 14:07:16 +0200

    remove vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['@mui/icons-material/Home'],
-    },
-    outDir: 'dist',
-  },
   plugins: [react()],
-  loader: { '.js': 'jsx' },
-  define: {
-    global: 'globalThis',
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': 'http://localhost:3000',
-      '/api/file': 'http://localhost:3000',
-    },
-  },
-});
+})
