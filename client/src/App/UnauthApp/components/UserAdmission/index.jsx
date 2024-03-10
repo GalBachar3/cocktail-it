9525d8f87b96437639a1dddea3bdedd91e9b6835
itay <itay.jacobson@gmail.com>
2024-03-10 21:24:58 +0200

    exterior design

--- client/src/App/UnauthApp/components/UserAdmission/index.jsx
+++ client/src/App/UnauthApp/components/UserAdmission/index.jsx
@@ -1,11 +1,15 @@
-import { Paper, Typography, useTheme } from '@mui/material';
+import { Grid, Paper, Typography, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { Center, Column } from '../../../../Layout';
+import logo from '../../../../Assets/logo.png'
 
 const UserAdmission = ({ formTitle, to, message, FormContent }) => {
     const { palette: { mode } } = useTheme();
 
     return (
+        <Grid sx={{width: '100%', height:'100%',display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', backgroundColor: '#f9f5da'}}>
+        <img src={logo} alt='logo' height='100px' width='400px' style={{alignSelf: 'center'}}></img>
+        <br/>
         <Paper elevation={5} sx={{ width: 400, padding: 2 }}>
             <Column sx={{ gap: 2, alignItems: 'center' }}>
                 <Typography fontSize={30}>{formTitle}</Typography>
@@ -19,6 +23,8 @@ const UserAdmission = ({ formTitle, to, message, FormContent }) => {
                 </Center>
             </Column>
         </Paper>
+        </Grid>
+        
     );
 };
 
