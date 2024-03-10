2caca6df94aa000bd668c976abf27860ee6a34cc
itay <itay.jacobson@gmail.com>
2024-03-11 00:49:31 +0200

    appbar

--- client/src/App/AuthApp/components/AppBar/index.jsx
+++ client/src/App/AuthApp/components/AppBar/index.jsx
@@ -1,15 +1,18 @@
-import { AppBar as MuiAppBar, Typography } from '@mui/material';
+import { Box, AppBar as MuiAppBar, Typography } from '@mui/material';
 import { Row } from '../../../../Layout';
 import { useUser } from '../../../../Providers/UserProvider';
 import UserMenu from './UserMenu';
+import logo from '../../../../Assets/logo.png'
 
 const AppBar = () => {
     const { user } = useUser();
 
     return (
-        <MuiAppBar position="static" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <MuiAppBar position="static" sx={{backgroundColor:'secondary.main' ,zIndex: (theme) => theme.zIndex.drawer + 1 }}>
             <Row sx={{ width: 1, padding: 1, alignItems: 'center' }}>
-                <Typography variant='h6' sx={{ flex: 1 }}>Hello, {user?.name}</Typography>
+                <img src={logo} alt='logo' height='50px' width='150px' style={{alignSelf: 'center'}}></img>
+                <Box sx={{ flex: 1 }}/>
+                <Typography variant='h6' >Hello, {user?.name}</Typography>
                 <UserMenu />
             </Row>
         </MuiAppBar>
